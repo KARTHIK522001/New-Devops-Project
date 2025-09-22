527e729b61c1101b454c2fb48af2c62eaff62532
KARTHIK <122384867+KARTHIK522001@users.noreply.github.com>
2025-09-22 22:36:06 +0530

    Update Myshellscript.sh

--- Myshellscript.sh
+++ Myshellscript.sh
@@ -4,5 +4,5 @@ echo "Hello world"
 mkdir -p newfolder 
 cd newfolder
 touch file2.txt
-echo "this is my first script in file\nThis is the my second line change\n this is my third line of comment" > file2.txt
+echo -e "this is my first script in file\nThis is the my second line change\n this is my third line of comment" > file2.txt
 cat file2.txt
